bc3837cc2a5d47e7d130542d00439de6b8e25465
David Asaf <dasaf3@gatech.edu>
2018-12-08 19:32:50 -0500

    fixing env bug

--- atd-graphql-server/server.ts
+++ atd-graphql-server/server.ts
@@ -5,7 +5,7 @@ import postgraphile from 'postgraphile';
 const app = express();
 const port = process.env.PORT || 5000;
 
-const DB_HOST = process.env.DB_DATABASE;
+const DB_HOST = process.env.DB_HOST;
 const DB_PORT = process.env.DB_PORT ? process.env.DB_PORT : 5432;
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
