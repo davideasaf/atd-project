12256019ac80074df83caadf98e6623a92aba384
David Asaf <dasaf3@gatech.edu>
2018-12-08 19:21:35 -0500

    updating to use correct env variables

--- atd-graphql-server/server.ts
+++ atd-graphql-server/server.ts
@@ -4,7 +4,15 @@ import postgraphile from 'postgraphile';
 
 const app = express();
 const port = process.env.PORT || 5000;
-const DATABASE_URL = process.env.DATABASE_URL;
+
+const DB_HOST = process.env.DB_DATABASE;
+const DB_PORT = process.env.DB_PORT ? process.env.DB_PORT : 5432;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_DATABASE = process.env.DB_DATABASE;
+
+const databaseConnString = `postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+console.log('databaseConnString:', databaseConnString);
 
 const corsOptions = {
   origin: '*',
@@ -15,10 +23,6 @@ const corsOptions = {
 app.options('*', cors());
 app.use(cors(corsOptions));
 
-app.use(
-  postgraphile(DATABASE_URL, {
-    graphiql: true,
-  }),
-);
+app.use(postgraphile(databaseConnString, { graphiql: true }));
 
 app.listen(port), () => console.log(`listening on ${port}`);
